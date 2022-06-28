5013bcebdb6573b0f393cbb2871bdc355db9f820
Vitaly Rodin <vitalyrweb@gmail.com>
2022-06-28 19:37:00 +0300

    add loops.js
    for a loop lesson

--- js/main.js
+++ js/main.js
@@ -1,23 +1,26 @@
-const numbersOfFilms = +prompt('Number of films you seen?', '');
+// const numbersOfFilms = +prompt('Number of films you seen?', '');
 
 
-const personalMovieDB = {
-    count: {},
-    movies: {},
-    actors: {},
-    genres: [],
-    privet: false
-}
+// const personalMovieDB = {
+//     count: {},
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privet: false
+// }
 
 
-const q1 = prompt('last movie you watched?', ''),
-    q2 = +prompt('value the movie from 1 - 10', ''),
-    q3 = prompt('last movie you watched?', ''),
-    q4 = +prompt('value the movie from 1 - 10', '');
+// const q1 = prompt('last movie you watched?', ''),
+//     q2 = +prompt('value the movie from 1 - 10', ''),
+//     q3 = prompt('last movie you watched?', ''),
+//     q4 = +prompt('value the movie from 1 - 10', '');
 
-personalMovieDB.movies[q1] = q2
-personalMovieDB.movies[q3] = q4
+// personalMovieDB.movies[q1] = q2
+// personalMovieDB.movies[q3] = q4
 
 
 
-console.log(personalMovieDB.movies);
+// console.log(personalMovieDB.movies);
+
+
+
