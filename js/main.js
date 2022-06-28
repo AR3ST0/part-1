f493fd44e7e1d73a4893a6a9f8911392739456e6
Vitaly Rodin <vitalyrweb@gmail.com>
2022-06-28 19:06:30 +0300

    Update main.js
    in main.js exercise No1'

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const numbersOfFilms = +prompt('Number of films you seen?', '');
+
+
+const personalMovieDB = {
+    count: {},
+    movies: {},
+    actors: {},
+    genres: [],
+    privet: false
+}
+
+
+const q1 = prompt('last movie you watched?', ''),
+    q2 = +prompt('value the movie from 1 - 10', ''),
+    q3 = prompt('last movie you watched?', ''),
+    q4 = +prompt('value the movie from 1 - 10', '');
+
+personalMovieDB.movies[q1] = q2
+personalMovieDB.movies[q3] = q4
+
+
+
+console.log(personalMovieDB.movies);
