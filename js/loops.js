69799f17c7ddb7515516db367110f47063fa58cb
Vitaly Rodin <vitalyrweb@gmail.com>
2022-06-28 19:56:17 +0300

    Update loops.js
    loops exs.

---
+++ js/loops.js
@@ -0,0 +1,38 @@
+
+//loops
+///////////////////////////////////////////////////
+// for (let i = 1; i < 10; i++) {
+//     if (i === 6) {
+//         // break;
+//         continue;
+
+//     }
+//     console.log(i);
+// }
+
+
+// loops in loops
+///////////////////////////////////////////////////
+// for (let i = 0; i < 3; i++) {
+//     console.log(i);
+//     for (let j = 0; j < 3; j++) {
+//         console.log(j + 'i');
+
+//     }
+// }
+
+
+
+// loop exs...
+///////////////////////////////////////////////////
+let result = ''
+
+const length = 7
+
+for (let i = 1; i < length; i++) {
+    for (let j = 0; j < i; j++) {
+        result += '*'
+    }
+    result += '/n'
+}
+document.write(result)
